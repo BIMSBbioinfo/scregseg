9cbaa45461c76cdd41e3b92e4b09850aeb028221
wkopp <wolfgang.kopp@mdc-berlin.de>
2021-01-03 00:07:31 +0100

    removed MultinomialHMM

--- src/scregseg/__init__.py
+++ src/scregseg/__init__.py
@@ -1,8 +1,5 @@
 __version__ = '0.0.0'
 
 from .scregseg import Scregseg  # noqa
-from .hmm import MultinomialHMM  # noqa
 from .hmm import DirMulHMM  # noqa
 from scregseg.motifs import MotifExtractor
-#from .hmm import MincovDirMulHMM  # noqa
-#from .hmm import DirMulMixture  # noqa
